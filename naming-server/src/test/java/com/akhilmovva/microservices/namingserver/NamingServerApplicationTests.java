2c1f77302a55a4f22cff67a9f34866a3101314e8
Akhil Movva <akhilmovva6852@gmail.com>
2021-03-13 19:19:52 -0500

    Add files via upload

---
+++ naming-server/src/test/java/com/akhilmovva/microservices/namingserver/NamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akhilmovva.microservices.namingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
